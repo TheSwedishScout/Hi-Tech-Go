73ad0991fbcbef97bd93a9ccf1e89bd8e2cbee77
carinaher <carina.herzog@gmail.com>
2016-10-04 03:55:17 +0200

    beer.js cleanup

--- geoTest/beer/js/beer.js
+++ geoTest/beer/js/beer.js
@@ -2,17 +2,7 @@
 function allowDrop(ev) {
     ev.preventDefault();
 }
-/*
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
 
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
-*/
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
     ev.dataTransfer.setData("data-id", ev.target.getAttribute("data-id"));
@@ -30,36 +20,4 @@ function drop(ev) {
     }
     
 }
-
-/*
-function draw(image) {
-    var gameDiv = document.createElement("div"); //create <div> </div>
-    gameDiv.className = "beerGame"; //adds class="beerGame" to the div-tags
-    
-    var logo1 = document.createElement("div");
-    logo1.textContent = image.name;
-    gameDiv.appendChild(logo1);
-    
-    var logo2 = document.createElement("div"); 
-    logo2.textContent = image.phone;
-    gameDiv.appendChild(logo2);
-    
-    var logo3 = document.createElement("div");
-    logo3.textContent = image.email;
-    gameDiv.appendChild(logo3);
-    
-    document.body.appendChild(gameDiv);
-}
-*/
-
-/*
-function main() {
-  gameDiv = document.createElement("div");
-  gameDiv.width = WIDTH;
-  gameDiv.height= HEIGHT;
-  document.body.appendChild(gameDiv);
-  
-main();
-
-*/
 
